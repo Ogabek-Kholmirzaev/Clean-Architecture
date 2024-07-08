db47f3a76e7e7893f4db7fa7fd2cacc199108ed6
Ogabek Kholmirzaev <xolmirzayev.ogabek.2004@gmail.com>
2024-07-08 12:04:55 +0500

    Application Layer

---
+++ Bookify/Bookify.Domain/Reviews/Rating.cs
@@ -0,0 +1,22 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Reviews;
+
+public sealed record Rating
+{
+    public static readonly Error InValid = new("Rating.InValid", "The rating is invalid");
+
+    private Rating(int value) => Value = value;
+
+    public int Value { get; init; }
+
+    public static Result<Rating> Create(int value)
+    {
+        if (value < 1 || value > 5)
+        {
+            return Result.Failure<Rating>(InValid);
+        }
+
+        return new Rating(value);
+    }
+}
